a934dfe6ecd2c969ce55dc21afd942c75bda1c28
varshney-himanshu <himanshu1998delhi@gmail.com>
2020-01-13 15:55:25 +0530

    portflio initial files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./sass/main.scss";
+import React, { Component } from "react";
+import logo from "./assets/signature-white.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div class="nav">
+          <input
+            id="nav-toggle"
+            type="checkbox"
+            class="nav__checkbox"
+            // eslint-disable-next-line no-script-url
+            href="javascript:void()"
+          />
+          <label class="nav__button" for="nav-toggle">
+            Menu
+          </label>
+          <div class="nav__hidden-div">
+            <div class="nav__links">
+              <a href="!#">HOME</a>
+              <a href="!#">CONTACT</a>
+              <a href="!#">DESIGN</a>
+              <a href="!#">WEBSITES</a>
+            </div>
+          </div>
+        </div>
+
+        <section id="header" class="header">
+          <img class="header__logo" src={logo} alt="logo-dark" />
+        </section>
+
+        <section class="about">
+          <h1>Hi there. I am Himanshu Varshney</h1>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
